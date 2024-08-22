7f5c61851ca62a2234798e3888c526bf0d57bb76
aditya172926 <aditya26sg@gmail.com>
2024-08-22 11:58:10 +0530

    fix: axios security update, fix aave v3 subgraph url

--- scripts/constants/simulation.ts
+++ scripts/constants/simulation.ts
@@ -1,4 +1,4 @@
-export const AAVE_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/aave/protocol-v3';
+export const AAVE_SUBGRAPH_BASEURL = 'https://gateway.thegraph.com/api';
 export const WBTC_ADDRESS = '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599';
 export const POOL_ADDRESS_PROVIDER_ADDRESS = '0x2f39d218133AFaB8F2B819B1066c7E434Ad94E9e';
 export const uiPoolDataProviderV3Address = '0x91c0eA31b49B69Ea18607702c5d9aC360bf3dE7d';
